b33ae4d4358b077f548b06061b00185620425968
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-09-02 20:26:34 +0600

    second solution added

--- 570. Managers with at Least 5 Direct Reports.sql
+++ 570. Managers with at Least 5 Direct Reports.sql
@@ -2,3 +2,8 @@ Method 1:
 SELECT name FROM Employee 
 WHERE id  IN (SELECT managerId FROM Employee GROUP BY managerId HAVING COUNT(*)>=5 )
 Method 2:
+SELECT e.name FROM Employee e 
+JOIN Employee m
+ON e.id = m.managerId 
+GROUP BY m.managerId
+HAVING COUNT(*) >= 5
