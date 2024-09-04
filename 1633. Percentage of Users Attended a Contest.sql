08ae691e2ed3e3cb02f11624684005567fc96493
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-09-04 11:24:39 +0600

    Second Solution added

--- 1633. Percentage of Users Attended a Contest.sql
+++ 1633. Percentage of Users Attended a Contest.sql
@@ -9,3 +9,12 @@ GROUP BY
 ORDER BY 
     percentage DESC, 
     r.contest_id ASC;
+
+Method 2:
+SELECT r.contest_id , ROUND((COUNT(DISTINCT r.user_id))/(SELECT COUNT(DISTINCT user_id) FROM Users)*100, 2) AS percentage
+FROM Users u 
+JOIN Register r
+ON u.user_id = r.user_id
+GROUP BY contest_id
+ORDER BY percentage DESC, r.contest_id ASC
+
